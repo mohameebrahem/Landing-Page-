2cf7738cf4eca85c2faa8410b595ef041599d9ba
mohamedEbrahem <mohamedebrahemprogrammer@gmail.com>
2023-03-10 07:12:14 +0200

    change some settings

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-let mainColors = localStorage.getItem('color-option');
+let localColors = localStorage.getItem('color-option');
 const colorsLi = document.querySelectorAll('.colors-list li');
-
-if (mainColors !== null) {
-  document.documentElement.style.setProperty('--main-color', mainColors);
+let intervalId;
+let backgroundOption = true;
+if (localColors !== null) {
+  document.documentElement.style.setProperty('--main-color', localColors);
 
   colorsLi.forEach((el) => {
     el.classList.remove('active');
-    if (el.dataset.color === mainColors) {
+    if (el.dataset.color === localColors) {
       el.classList.add('active');
     }
   });
@@ -32,10 +33,10 @@ colorsLi.forEach((li) => {
 });
 const landingPage = document.querySelector('.landing-page');
 let imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
-let intervalId;
+
 const backgroundBtns = document.querySelectorAll('.random-backgrounds span');
 function randomnbackgrounds() {
-  if (!intervalId) {
+  if (backgroundOption == true) {
     intervalId = setInterval(() => {
       let randomNumber = Math.floor(Math.random() * imagesArray.length);
 
@@ -51,10 +52,32 @@ backgroundBtns.forEach((btn) => {
     e.target.classList.add('active');
 
     if (e.target.classList.contains('yes')) {
+      backgroundOption = true;
       randomnbackgrounds();
+      localStorage.setItem('background-option', true);
     } else {
       clearInterval(intervalId);
+      backgroundOption == false;
       intervalId = null;
+      localStorage.setItem('background-option', false);
     }
   });
 });
+
+let localBackground = localStorage.getItem('background-option');
+if (localBackground) {
+  if (localBackground == 'true') {
+    backgroundOption = true;
+  } else if (localBackground == 'false') {
+    backgroundOption = false;
+  }
+  backgroundBtns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+  if (localBackground == 'true') {
+    document.querySelector('.yes').classList.add('active');
+  } else if (localBackground == 'false') {
+    document.querySelector('.no').classList.add('active');
+  }
+}
+randomnbackgrounds();
