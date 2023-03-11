dbdcc50c0e720addf6c46295229cb34f6398e971
mohamedEbrahem <mohamedebrahemprogrammer@gmail.com>
2023-03-11 08:06:20 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let localColors = localStorage.getItem('color-option');
 const colorsLi = document.querySelectorAll('.colors-list li');
+const allSkills = document.querySelectorAll('.skill-box span');
 let intervalId;
 let backgroundOption = true;
 if (localColors !== null) {
@@ -25,10 +26,7 @@ colorsLi.forEach((li) => {
       e.target.dataset.color
     );
     localStorage.setItem('color-option', e.target.dataset.color);
-    e.target.parentElement.querySelectorAll('.active').forEach((el) => {
-      el.classList.remove('active');
-    });
-    e.target.classList.add('active');
+    handleActive(e);
   });
 });
 const landingPage = document.querySelector('.landing-page');
@@ -44,26 +42,6 @@ function randomnbackgrounds() {
     }, 1000);
   }
 }
-backgroundBtns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    e.target.parentElement.querySelectorAll('span').forEach((span) => {
-      span.classList.remove('active');
-    });
-    e.target.classList.add('active');
-
-    if (e.target.classList.contains('yes')) {
-      backgroundOption = true;
-      randomnbackgrounds();
-      localStorage.setItem('background-option', true);
-    } else {
-      clearInterval(intervalId);
-      backgroundOption == false;
-      intervalId = null;
-      localStorage.setItem('background-option', false);
-    }
-  });
-});
-
 let localBackground = localStorage.getItem('background-option');
 if (localBackground) {
   if (localBackground == 'true') {
@@ -80,4 +58,92 @@ if (localBackground) {
     document.querySelector('.no').classList.add('active');
   }
 }
-randomnbackgrounds();
+backgroundBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    handleActive(e);
+
+    if (e.target.classList.contains('yes')) {
+      backgroundOption = true;
+      randomnbackgrounds();
+      localStorage.setItem('background-option', true);
+    } else {
+      clearInterval(intervalId);
+      backgroundOption == false;
+      intervalId = null;
+      localStorage.setItem('background-option', false);
+    }
+  });
+});
+
+let ourSkills = document.querySelector('.skills');
+window.onscroll = function () {
+  let skillsOffsetTop = ourSkills.offsetTop;
+  let skillsOffsetOuterHeight = ourSkills.offsetHeight;
+  let innerHeight = this.innerHeight;
+  let windowScrollTop = this.scrollY;
+
+  if (
+    windowScrollTop >
+    skillsOffsetTop + skillsOffsetOuterHeight - innerHeight * 1.5
+  ) {
+    allSkills.forEach((element) => {
+      element.style.width = element.dataset.progress;
+    });
+  } else {
+    allSkills.forEach((element) => {
+      element.style.width = 0;
+    });
+  }
+};
+let ourGallery = document.querySelectorAll('.images-box img');
+ourGallery.forEach((img) => {
+  img.addEventListener('click', (e) => {
+    let overlay = document.createElement('div');
+    overlay.className = 'popup-overlay';
+    document.body.appendChild(overlay);
+    let popupBox = document.createElement('div');
+    popupBox.className = 'popup-box';
+
+    if (img.alt != null) {
+      let popupHeader = document.createElement('h3');
+      popupHeader.textContent = img.alt;
+      popupBox.appendChild(popupHeader);
+    }
+
+    let popupImage = document.createElement('img');
+    popupImage.src = img.src;
+    popupBox.appendChild(popupImage);
+    document.body.appendChild(popupBox);
+    let closeBtn = document.createElement('span');
+    closeBtn.textContent = 'x';
+    closeBtn.className = 'close-button ';
+    popupBox.appendChild(closeBtn);
+  });
+});
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('close-button')) {
+    document.querySelector('.popup-overlay').remove();
+    document.querySelector('.popup-box').remove();
+  }
+});
+
+const allBullets = document.querySelectorAll('.nav-bullets .bullet');
+const allLinks = document.querySelectorAll('.links a');
+function scrollToTarget(elements) {
+  elements.forEach((element) => {
+    element.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.querySelector(e.target.dataset.section).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+}
+const handleActive = function (e) {
+  e.target.parentElement.querySelectorAll('.active').forEach((element) => {
+    element.classList.remove('active');
+  });
+  e.target.classList.add('active');
+};
+scrollToTarget(allBullets);
+scrollToTarget(allLinks);
